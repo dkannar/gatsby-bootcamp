95d3a1d849df2befb9d78fe80f524ae7d00e614a
Daniel Kannar <danielkannar@icloud.com>
2020-02-14 14:16:56 +0100

    Created layout for Gatsby blog, added Styles and Sitemetada through GraphQL

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About</h1>
+      <p>Daniel's blog created by the Great Gatsby.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>I'm Daniel, a full-stack developer, living in beautiful Budapest.</h2>
+    </Layout>
+  )
+}
+
+export default IndexPage
