6ab42291f6a22253b95f2a4d2143a3905ae2b061
Daniel Kannar <danielkannar@icloud.com>
2020-02-14 23:20:43 +0100

    Added blogposts and created slugs

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,6 @@ const Header = () => {
       site {
         siteMetadata {
           title
-          author
         }
       }
     }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,42 @@
 import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
       <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+              </Link>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
       <p>Posts will show up here later on.</p>
     </Layout>
   )
